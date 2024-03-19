922c3a7bb74458c07f31549d9972ef18698e0d0c
Renan Puppo Furtado <renan.puppo@gmail.com>
2024-03-19 20:49:05 -0300

    initial version

---
+++ src/main/java/br/com/pupposoft/fiap/SgpUsuarioService.java
@@ -0,0 +1,17 @@
+package br.com.pupposoft.fiap;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class })
+public class SgpUsuarioService {
+    public static void main(String[] args) {
+    	Locale.setDefault(new Locale( "pt", "BR" ));
+        SpringApplication.run(SgpUsuarioService.class, args);
+    }
+}
---
+++ src/main/java/br/com/pupposoft/fiap/sgp/usuario/config/exception/ControllerExceptionHandlerConfiguration.java
@@ -0,0 +1,37 @@
+package br.com.pupposoft.fiap.sgp.usuario.config.exception;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import br.com.pupposoft.fiap.starter.exception.ExceptionJson;
+import br.com.pupposoft.fiap.starter.exception.SystemBaseException;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class ControllerExceptionHandlerConfiguration {
+
+	@ExceptionHandler({ SystemBaseException.class})
+	@ResponseBody
+	public ResponseEntity<ExceptionJson> sasException(final SystemBaseException e, final HttpServletResponse response) {
+		final ExceptionJson exceptionJson = new ExceptionJson(e);
+		return new ResponseEntity<>(exceptionJson, new HttpHeaders(), e.getHttpStatus());
+	}
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(Throwable.class)
+	@ResponseBody
+	public ExceptionJson genericError(final Throwable e) {
+		log.error(e.getMessage(), e);
+		return new ExceptionJson(HttpStatus.INTERNAL_SERVER_ERROR.name(), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+	}
+}
